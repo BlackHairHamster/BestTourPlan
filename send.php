0a986bea0d6aedf89dd3d9cdf6ddfd17c70c8738
BlackHairHamster <ivanfomin2005@gmail.com>
2020-08-06 12:10:39 +0300

    added: validation + validation.mask

---
+++ send.php
@@ -0,0 +1,63 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$mail = $_POST['email'];
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
+// Формирование самого письма
+$title = "Новое обращение - Best Tour Plan";
+$body = "
+<h2>Новое обращение</h2>
+<b>Имя:</b> $name<br>
+<b>Телефон:</b> $phone<br><br>
+<b>Сообщение:</b><br>$message<br>
+<b>Почта:</b> $mail
+";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    // $mail->SMTPDebug = 2;
+    $mail->Debugoutput = function ($str, $level) {
+        $GLOBALS['status'][] = $str;
+    };
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.yandex.ru'; // SMTP сервера вашей почты
+    $mail->Username   = 'FireHairHamster@yandex.ru'; // Логин на почте
+    $mail->Password   = '89153163717'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('FireHairHamster@yandex.ru', 'Дмитрий Петров'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('FireHairHamster@yandex.ru');
+
+    // Отправка сообщения
+    $mail->isHTML(true);
+    $mail->Subject = $title;
+    $mail->Body = $body;
+
+    // Проверяем отравленность сообщения
+    if ($mail->send()) {
+        $result = "success";
+        header('Location: thankyou-footer.html');
+    } else {
+        $result = "error";
+        header('Location: error.html');
+    }
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
